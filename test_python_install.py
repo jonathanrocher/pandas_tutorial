d3821166d58df14acb49b6077cd98d0384f0e189
Jonathan Rocher <jrocher@enthought.com>
2015-06-29 15:51:57 -0500

    Remove hdf5 from dependencies.

--- test_python_install.py
+++ test_python_install.py
@@ -22,11 +22,6 @@ def test_import_matplotlib():
     import matplotlib
 
 
-def test_load_h5():
-    from pandas import read_hdf
-    read_hdf(join(HERE, "data", "greenhouse_gaz", "co2_mm_global.h5"), "table")
-
-
 def test_scrape_web():
     import pandas as pd
     pd.read_html("http://en.wikipedia.org/wiki/World_population")
